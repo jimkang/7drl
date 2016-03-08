be8de5795ca33f021286c3db42679a08daa7e78d
Jim Kang <jimkang@gmail.com>
2016-03-07 22:17:18 -0500

    Generate graphs with randomgraph.js/Watts-Strogatz Beta.

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-var div = document.createElement('div');
-div.innerText = 'Oh hay you need to make an app here.'
-document.body.appendChild(div);
+var Graph = require('../graph');
+var randomgraph = require('randomgraph');
+
+var graphData = randomgraph.WattsStrogatz.beta(40, 4, 0.2);
+
+var graph = Graph({
+  width: 960,
+  height: 500
+});
+
+graph.render();
+graph.renderUpdate(graphData.nodes, graphData.edges);
